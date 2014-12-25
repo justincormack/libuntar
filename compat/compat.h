d331aa164fd27eb4e678499532753063dacccff0
Justin Cormack <justin@specialbusservice.com>
2014-12-25 21:36:58 +0000

    remove glob support

--- compat/compat.h
+++ compat/compat.h
@@ -88,68 +88,6 @@ int compat_getservbyname_r(const char *, const char *, struct servent *,
 
 
 
-#ifdef NEED_GLOB
-# ifndef HAVE_GLOB
-
-typedef struct {
-	int gl_pathc;		/* Count of total paths so far. */
-	int gl_matchc;		/* Count of paths matching pattern. */
-	int gl_offs;		/* Reserved at beginning of gl_pathv. */
-	int gl_flags;		/* Copy of flags parameter to glob. */
-	char **gl_pathv;	/* List of paths matching pattern. */
-				/* Copy of errfunc parameter to glob. */
-	int (*gl_errfunc)(const char *, int);
-
-	/*
-	 * Alternate filesystem access methods for glob; replacement
-	 * versions of closedir(3), readdir(3), opendir(3), stat(2)
-	 * and lstat(2).
-	 */
-	void (*gl_closedir)(void *);
-	struct dirent *(*gl_readdir)(void *);
-	void *(*gl_opendir)(const char *);
-	int (*gl_lstat)(const char *, struct stat *);
-	int (*gl_stat)(const char *, struct stat *);
-} glob_t;
-
-/* Flags */
-#  define GLOB_APPEND	0x0001	/* Append to output from previous call. */
-#  define GLOB_DOOFFS	0x0002	/* Use gl_offs. */
-#  define GLOB_ERR	0x0004	/* Return on error. */
-#  define GLOB_MARK	0x0008	/* Append / to matching directories. */
-#  define GLOB_NOCHECK	0x0010	/* Return pattern itself if nothing matches. */
-#  define GLOB_NOSORT	0x0020	/* Don't sort. */
-
-#  define GLOB_ALTDIRFUNC 0x0040 /* Use alternately specified directory funcs. */
-#  define GLOB_BRACE	0x0080	/* Expand braces ala csh. */
-#  define GLOB_MAGCHAR	0x0100	/* Pattern had globbing characters. */
-#  define GLOB_NOMAGIC	0x0200	/* GLOB_NOCHECK without magic chars (csh). */
-#  define GLOB_QUOTE	0x0400	/* Quote special chars with \. */
-#  define GLOB_TILDE	0x0800	/* Expand tilde names from the passwd file. */
-#  define GLOB_NOESCAPE	0x1000	/* Disable backslash escaping. */
-
-/* Error values returned by glob(3) */
-#  define GLOB_NOSPACE	(-1)	/* Malloc call failed. */
-#  define GLOB_ABORTED	(-2)	/* Unignored error. */
-#  define GLOB_NOMATCH	(-3)	/* No match and GLOB_NOCHECK not set. */
-#  define GLOB_NOSYS	(-4)	/* Function not supported. */
-#  define GLOB_ABEND	GLOB_ABORTED
-
-int openbsd_glob(const char *, int, int (*)(const char *, int), glob_t *);
-void openbsd_globfree(glob_t *);
-#  define glob openbsd_glob
-#  define globfree openbsd_globfree
-
-# else /* HAVE_GLOB */
-
-#  ifdef HAVE_GLOB_H
-#   include <glob.h>
-#  endif
-
-# endif /* ! HAVE_GLOB */
-#endif /* NEED_GLOB */
-
-
 #if defined(NEED_INET_ATON) && !defined(HAVE_INET_ATON)
 
 int inet_aton(const char *, struct in_addr *);
