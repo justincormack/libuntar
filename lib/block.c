8fa418c5bec879fb4b6215122c62ff68790e3ace
Justin Cormack <justin@specialbusservice.com>
2015-06-11 15:59:44 +0000

    merge util into block

--- lib/block.c
+++ lib/block.c
@@ -3,8 +3,6 @@
 **  Copyright 1998-2003 Mark D. Roth
 **  All rights reserved.
 **
-**  block.c - libtar code to handle tar archive header blocks
-**
 **  Mark D. Roth <roth@uiuc.edu>
 **  Campus Information Technologies and Educational Services
 **  University of Illinois at Urbana-Champaign
@@ -19,6 +17,53 @@
 #include <sys/param.h>
 #include <pwd.h>
 #include <grp.h>
+
+/*
+**  util.c - miscellaneous utility code for libtar
+*/
+
+/* calculate header checksum */
+int
+th_crc_calc(TAR *t)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < T_BLOCKSIZE; i++)
+		sum += ((unsigned char *)(&(t->th_buf)))[i];
+	for (i = 0; i < 8; i++)
+		sum += (' ' - (unsigned char)t->th_buf.chksum[i]);
+
+	return sum;
+}
+
+
+/* calculate a signed header checksum */
+int
+th_signed_crc_calc(TAR *t)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < T_BLOCKSIZE; i++)
+		sum += ((signed char *)(&(t->th_buf)))[i];
+	for (i = 0; i < 8; i++)
+		sum += (' ' - (signed char)t->th_buf.chksum[i]);
+
+	return sum;
+}
+
+
+/* string-octal to integer conversion */
+int
+oct_to_int(char *oct)
+{
+	int i;
+
+	return sscanf(oct, "%o", &i) == 1 ? i : 0;
+}
+
+/*
+**  block.c - libtar code to handle tar archive header blocks
+*/
 
 #define BIT_ISSET(bitmask, bit) ((bitmask) & (bit))
 
