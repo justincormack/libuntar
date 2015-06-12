6bae54fc1d557cfa93934c4fe261338f59154470
Justin Cormack <justin@specialbusservice.com>
2015-06-12 10:12:33 +0000

    reduce to one file

--- lib/untar.c
+++ lib/untar.c
@@ -927,3 +927,87 @@ tar_extract_fifo(TAR *t, char *filename)
 
 	return 0;
 }
+
+/*
+**  handle.c - libtar code for initializing a TAR handle
+*/
+
+static tartype_t default_type = { open, close, read };
+
+static int
+tar_init(TAR **t, tartype_t *type,
+	 int oflags, int mode, int options)
+{
+	if ((oflags & O_ACCMODE) == O_RDWR)
+	{
+		errno = EINVAL;
+		return -1;
+	}
+
+	*t = (TAR *)calloc(1, sizeof(TAR));
+	if (*t == NULL)
+		return -1;
+
+	(*t)->options = options;
+	(*t)->type = (type ? type : &default_type);
+	(*t)->oflags = oflags;
+	(*t)->dirfd = AT_FDCWD;
+	(*t)->atflags = AT_SYMLINK_NOFOLLOW;
+
+	return 0;
+}
+
+/* open a new tarfile handle */
+int
+tar_open(TAR **t, const char *pathname, tartype_t *type,
+	 int oflags, int mode, int options)
+{
+	if (tar_init(t, type, oflags, mode, options) == -1)
+		return -1;
+
+	if ((options & TAR_NOOVERWRITE) && (oflags & O_CREAT))
+		oflags |= O_EXCL;
+
+	(*t)->fd = (*((*t)->type->openfunc))(pathname, oflags, mode);
+	if ((*t)->fd == -1)
+	{
+		free(*t);
+		return -1;
+	}
+
+	return 0;
+}
+
+
+int
+tar_fdopen(TAR **t, int fd, const char *pathname, tartype_t *type,
+	   int oflags, int mode, int options)
+{
+	if (tar_init(t, type, oflags, mode, options) == -1)
+		return -1;
+
+	(*t)->fd = fd;
+	return 0;
+}
+
+int
+tar_fd(TAR *t)
+{
+
+	return t->fd;
+}
+
+/* close tarfile handle */
+int
+tar_close(TAR *t)
+{
+	int i;
+
+	i = (*(t->type->closefunc))(t->fd);
+
+	if (t->th_pathname != NULL)
+		free(t->th_pathname);
+	free(t);
+
+	return i;
+}
